a08a2708741c6dca7816d39813a1701ad45b8192
unknown <frankortiz5249@gmail.com>
2023-06-06 22:16:59 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,26 @@
 const router = require('express').Router()
+const places = require('../modules/places.js')
 
 router.get("/new", (req, res) => {
   res.render("places/new");
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.semd('POST /places')
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city){
+    req.body.city = "Anytown"
+  }
+  if (!req.body.state) {
+    req.body.state = "USA"
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/Tai-Food.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/Cat-Foam-Art.jpg'
-      }]
-
+    
     res.render('places/index', {places})
 })
 
