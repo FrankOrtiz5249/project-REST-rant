b61b2de758fe740f1a6956e5b7a6cc401c6dd337
unknown <frankortiz5249@gmail.com>
2023-06-06 21:31:56 -0700

    adding bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" ></img>
+                </div>
+                <div>
+                    Photo by <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a> on <a href="https://unsplash.com/photos/MsTOg6rhRVk">Unsplash</a>
+                </div>
                     <a href="/places">
                         <button className="btn-primary">Places Page</button>
                     </a>
