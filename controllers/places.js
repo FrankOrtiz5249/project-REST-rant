935b43daf89fb74db28a965af1ad3aa2a5754d41
unknown <frankortiz5249@gmail.com>
2023-06-06 22:05:43 -0700

    form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,11 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.semd('POST /places')
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
