ac2c95e90825ea79f94817c44dbb49bb15853dc1
unknown <frankortiz5249@gmail.com>
2023-06-06 22:48:18 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,7 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 })
 
+//post
 router.post('/', (req, res) => {
   if (!req.body.pic) {
     req.body.pic = 'http://placekitten.com/400/400'
@@ -19,9 +20,24 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+//index
 router.get('/', (req, res) => {
     
     res.render('places/index', {places})
+})
+
+//ID
+router.get('/:id', (req, res)=>{
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+  res.render('places/show', {place: places[id]})
+  }
 })
 
 module.exports = router
